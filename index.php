0c5cf5c5af177c5455ad71d71f9574b4173c51be
RemyHannequin <remy.hannequin@gmail.com>
2012-11-17 00:55:32 +0100

    Add index file to warn that the website is being developed.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Web development @ ESGI</title>
+  </head>
+  <body>
+
+    <h1>Site en construction ...</h1>
+
+  </body>
+
+</html>
