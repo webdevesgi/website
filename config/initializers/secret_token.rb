37f4eb98dd0632df641065d5e80694e93b3efbef
rhannequin <rhannequin@mybox.(none)>
2012-11-17 00:31:18 +0100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebDevEsgi::Application.config.secret_token = 'b44844506b6bb3ab7e4ffd2001783d08856eed6f0fd56b9cbd78a2ae6e614b8c528a63d4307a4622a107c90b8cb2fea0377792049c27ee37610d46e5088b6293'
